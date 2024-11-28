d36ed9271b67295fd13679eb7fe1993167c0804b
Soumendebnath123 <116148944+Soumendebnath123@users.noreply.github.com>
2024-11-28 18:54:14 +0530

    Add files via upload

---
+++ MenuApi.js
@@ -0,0 +1,132 @@
+const Menu = [
+    {
+      id: 1,
+      image: "image/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "A steaming bowl of comfort! Quick-cooked noodles tossed with a savory blend of spices, served hot and flavorful. Customize with your choice of veggies, cheese, or a spicy twist. Perfect for a light snack or a hearty meal. ",
+    },
+  
+    {
+      id: 2,
+      image: "image/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Crispy, golden delights! Thinly sliced potatoes dipped in a spiced gram flour batter and deep-fried to perfection. Served hot with tangy chutney, these savory fritters are a must-have for snack lovers and perfect with a cup of tea. ",
+    },
+    {
+      id: 3,
+      image: "image/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Char-grilled to perfection, this smoky delight is seasoned with a dash of tangy lemon, a sprinkle of salt, and a touch of spicy chili powder. A classic treat that's wholesome, flavorful, and satisfying. ",
+    },
+    {
+      id: 4,
+      image: "../image/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "A hearty and flavorful dish of tender chickpeas simmered in a rich, aromatic tomato-based gravy, seasoned with a perfect blend of spices. Served hot with naan, roti, or rice, it's a delicious and comforting meal option. ",
+    },
+    {
+      id: 5,
+      image: "../image/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "A delightful crispy crust topped with tangy tomato sauce, melted mozzarella, and your choice of fresh toppings like veggies, cheese, or meats. Baked to golden perfection, it's a slice of happiness for any time of the day. ",
+    },
+    {
+      id: 6,
+      image: "../image/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A hearty platter featuring a variety of flavors! Includes succulent chicken or mutton curry, aromatic biryani or steamed rice, flavorful dal, soft roti or naan, tangy pickles, fresh salad, and a sweet dessert to complete the feast. A perfect choice for meat lovers craving a wholesome meal. ",
+    },
+    {
+      id: 7,
+      image: "../image/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "A delightful treat to satisfy your cravings! Indulge in the perfect balance of flavor and sweetness, crafted to bring joy with every bite. Perfect for any occasion. ",
+    },
+    {
+      id: 8,
+      image: "../image/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "A comforting and wholesome dish featuring creamy, spiced kidney bean curry served over steaming hot rice. A North Indian classic that's hearty, flavorful, and perfect for a satisfying meal. ",
+    },
+    {
+      id: 9,
+      image: "../image/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Crispy, golden pastry pockets filled with a savory mixture of spiced potatoes, peas, and herbs. Served hot, these crispy treats are perfect as a snack or appetizer, paired with tangy tamarind chutney or mint sauce.",
+    },
+    {
+      id: 10,
+      image: "../image/vegthali.jpg",
+      name: "vegthali",
+      category: "dinner",
+      price: "140₹",
+      description:
+        "A wholesome platter featuring a variety of vegetarian delights! Includes a flavorful vegetable curry, dal, aromatic rice or roti, cooling raita, fresh salad, pickles, and a sweet treat to round off the meal. A perfect choice for a balanced and satisfying meal. ",
+    },
+    {
+      id: 11,
+      image: "../image/rotiwithmeat.jpg",
+      name: "rotiwithmeat",
+      category: "dinner",
+      price: "100₹",
+      description:
+        "Soft, warm, and freshly baked roti served alongside succulent, flavorful pieces of chicken, mutton, or beef in a rich, aromatic curry. A classic and comforting combination that pairs perfectly for a satisfying meal. ",
+    },
+    {
+      id: 12,
+      image: "../image/puri.jpg",
+      name: "puri",
+      category: "breakfast",
+      price: "90₹",
+      description:
+        "Fluffy, golden-brown deep-fried bread, crisp on the outside and soft on the inside. Served with a variety of accompaniments like aloo curry, chickpea masala, or even sweet syrup, puri is a delightful treat for breakfast or any festive occasion.",
+    },
+    {
+      id: 13,
+      image: "../image/momo.jpg",
+      name: "momo",
+      category: "evening",
+      price: "90₹",
+      description:
+        "Steamed or fried dumplings filled with a savory mix of vegetables or meat, seasoned with aromatic spices. Served with a tangy dipping sauce, momos are a popular snack or appetizer, loved for their soft, delicate texture and bold flavors. ",
+    },
+    {
+      id: 14,
+      image: "../image/paubhaji.jpg",
+      name: "paubhaji",
+      category: "evening",
+      price: "90₹",
+      description:
+        "A flavorful, spiced vegetable mash cooked to perfection, served with buttered, toasted pav (bread rolls). This Mumbai street food classic is a delightful combination of savory, tangy, and spicy flavors, perfect for a quick and satisfying meal. ",
+    },
+
+  ];
+  
+  export default Menu;
